f22d2c9a9a0b75b5982fe57dbff73143f3d2eb36
SunnyQjm <qjm253@pku.edu.cn>
2020-08-06 21:09:56 +0800

    add: 添加友链加载动效

--- src/pages/index/page1/index.tsx
+++ src/pages/index/page1/index.tsx
@@ -4,6 +4,7 @@ import {
     csdn,
     down, github, hexo, jianShu, logo, starrySky3, starrySky4, wordpress,
 } from '../../../imgs';
+import RcQueueAnim from "rc-queue-anim";
 import {AutoSwitchBg, CircleImageComponent, MeteorEffectBgComponent} from "../../../components";
 
 interface IndexPage1Props {
@@ -11,7 +12,7 @@ interface IndexPage1Props {
 }
 
 interface IndexPage1State {
-
+    showFriendLinks: boolean
 }
 
 export default class IndexPage1 extends React.PureComponent<IndexPage1Props, IndexPage1State> {
@@ -22,7 +23,17 @@ export default class IndexPage1 extends React.PureComponent<IndexPage1Props, Ind
         }
     }
 
+    constructor(props: IndexPage1Props) {
+        super(props);
+        this.state = {
+            showFriendLinks: false
+        };
+    }
+
     render() {
+        const {
+            showFriendLinks
+        } = this.state;
         return (
             <div className='index-page-1'>
                 {/*Index Page 1*/}
@@ -39,25 +50,31 @@ export default class IndexPage1 extends React.PureComponent<IndexPage1Props, Ind
 
                 <MeteorEffectBgComponent/>
 
-                <h1 className='index-page-1__title'>SunnyQjm | 建明</h1>
+                <h1 className='index-page-1__title' onClick={() => {
+                    this.setState({
+                        showFriendLinks: !showFriendLinks
+                    })
+                }} style={{
+                    zIndex: 1
+                }}>SunnyQjm | 建明</h1>
                 <span>每一个不曾起舞的日子，都是对生命的辜负</span>
-                <div className='index-page-1__friend-links'>
-                    <a href="https://github.com/SunnyQjm">
+                <RcQueueAnim className='index-page-1__friend-links' key='page' type={'scaleX'} delay={300} duration={5000}>
+                    <a href="https://github.com/SunnyQjm" key={'github'}>
                         <CircleImageComponent imgSrc={github} size={30} padding={10} backgroundColor={'white'}/>
                     </a>
-                    <a href="https://blog.csdn.net/qq_33215972">
+                    <a href="https://blog.csdn.net/qq_33215972" key={'csdn'}>
                         <CircleImageComponent imgSrc={csdn} size={30} padding={10} backgroundColor={'white'}/>
                     </a>
-                    <a href="https://sunnyqjm.github.io/">
+                    <a href="https://sunnyqjm.github.io/" key={'hexo'}>
                         <CircleImageComponent imgSrc={hexo} size={30} padding={10} backgroundColor={'white'}/>
                     </a>
-                    <a href="https://www.jianshu.com/u/d8e0ad691736">
+                    <a href="https://www.jianshu.com/u/d8e0ad691736" key={'jianshu'}>
                         <CircleImageComponent imgSrc={jianShu} size={30} padding={10} backgroundColor={'white'}/>
                     </a>
-                    <a href="https://blog.qjm253.cn/">
+                    <a href="https://blog.qjm253.cn/" key={'wordpress'}>
                         <CircleImageComponent imgSrc={wordpress} size={30} padding={10} backgroundColor={'white'}/>
                     </a>
-                </div>
+                </RcQueueAnim>
                 <img src={down} className='index-page-1__next-page' alt='下一页' onClick={this.props.onNextPageClick}/>
             </div>
         );
